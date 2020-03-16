3afb2d3b4f73b0892207575c134dfe941e75b787
Katerina <katerinalupacheva@gmail.com>
2020-03-16 14:50:45 +0300

    Cleanup 2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
+import {config } from './gapiConfig';
+
 function App() {
+  const[data, setData] = useState(null); 
+
+  const getData = () => {
+    window.gapi.client.sheets.spreadsheets.values.get({
+      spreadsheetId: config.SPREADSHEET_ID,
+      range: 'January!B2:E',
+    }).then(response => {
+      var range = response.result.values;
+      console.log('RESPONSE ' + JSON.stringify(range));
+    }, function(response) {
+      console.log('ERROR')
+    });
+  };
+
+  const initClient = async () => {
+    window.gapi.client.init({
+      apiKey: config.API_KEY,
+      clientId: config.CLIENT_ID,
+      discoveryDocs: config.DISCOVERY_DOCS,
+      scope: config.SCOPES
+    }).then(() => getData())
+    .catch((error) => {
+      console.log('ERROR ' + error.message)
+    });
+};
+  
+
+  useEffect(() => {
+    window.gapi.load('client:auth2', initClient);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
