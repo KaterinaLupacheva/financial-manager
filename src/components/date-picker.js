bf444557fe212b70832c17436f1581644bec6c6d
Katerina <katerinalupacheva@gmail.com>
2020-03-17 22:22:19 +0300

    Chose only month in the DatePicker

--- src/components/date-picker.js
+++ src/components/date-picker.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import { DatePicker, MuiPickersUtilsProvider } from '@material-ui/pickers';
 import DateFnsUtils from '@date-io/date-fns';
-import { format } from 'date-fns'
+import { format } from 'date-fns';
 
 const CustomDatePicker = ({changeDate}) => {
     return(
     <MuiPickersUtilsProvider utils={DateFnsUtils}>
         <DatePicker
-            value={new Date()}
-            onChange={(e) => changeDate(format(e, 'yyyy-MM-dd'))}
             minDate={new Date('2020-01-01')}
             maxDate={new Date()}
+            variant="inline"
+            openTo="month"
+            views={["year", "month"]}
+            value={new Date()}
+            onChange={(e) => changeDate(format(e, 'yyyy-MM-dd'))}
         />
     </MuiPickersUtilsProvider>
 )};
