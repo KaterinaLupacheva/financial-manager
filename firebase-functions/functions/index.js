076cb7b8ee346b21063ca5153d8b2499eab0ed6f
Katerina <katerinalupacheva@gmail.com>
2020-03-21 19:50:10 +0300

    Get and post endpoints added

--- firebase-functions/functions/index.js
+++ firebase-functions/functions/index.js
@@ -1,8 +1,54 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+admin.initializeApp();
+
+const express = require('express');
+const app = express();
+
+app.get('/finances', (req, res) => {
+    admin
+        .firestore()
+        .collection('finances')
+        .get()
+        .then((data) => {
+            let finances = [];
+            data.forEach(doc => {
+                finances.push({
+                    financeId: doc.id,
+                    sum: doc.data().sum,
+                    details: doc.data().details,
+                    category: doc.data().category,
+                    type: doc.data().type,
+                    date: doc.data().date
+                })
+            });
+            return res.json(finances);
+        })
+        .catch((err) => console.error(err)); 
 });
+
+app.post('/finance', (req, res) => {
+    const newFinance = {
+        date: admin.firestore.Timestamp.fromDate(new Date()),
+        sum: req.body.sum,
+        details: req.body.details,
+        category: req.body.category,
+        type: req.body.type
+    };
+
+    admin
+        .firestore()
+        .collection('finances')
+        .add(newFinance)
+        .then((doc) => {
+            res.json({ message: `document ${doc.id} created`})
+        })
+        .catch((err) => {
+            res.status(500).json({ error: 'something went wrong'})
+            console.error(err);
+        });
+});
+
+
+exports.api = functions.https.onRequest(app);
