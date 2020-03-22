f9db04039fbae906bcab510b1291fa2ca0ff043d
Katerina <katerinalupacheva@gmail.com>
2020-03-22 16:16:01 +0300

    Authorization middleware added

--- firebase-functions/functions/index.js
+++ firebase-functions/functions/index.js
@@ -40,13 +40,48 @@ app.get("/finances", (req, res) => {
     .catch(err => console.error(err));
 });
 
-app.post("/finance", (req, res) => {
+const FBAuth = (req, res, next) => {
+  let idToken;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer ")
+  ) {
+    idToken = req.headers.authorization.split("Bearer ")[1];
+  } else {
+    console.error("No token found");
+    return res.status(403).json({ error: "Unauthorized" });
+  }
+
+  admin
+    .auth()
+    .verifyIdToken(idToken)
+    .then(decodedToken => {
+      req.user = decodedToken;
+      console.log(decodedToken);
+      return db
+        .collection("/users")
+        .where("userId", "==", req.user.uid)
+        .limit(1)
+        .get();
+    })
+    .then(data => {
+      req.user.username = data.docs[0].data().username;
+      return next();
+    })
+    .catch(err => {
+      console.error("Error while verifying token ", err);
+      return res.status(403).json(err);
+    });
+};
+
+app.post("/finance", FBAuth, (req, res) => {
   const newFinance = {
     date: new Date().toISOString(),
     sum: req.body.sum,
     details: req.body.details,
     category: req.body.category,
-    type: req.body.type
+    type: req.body.type,
+    username: req.user.username
   };
 
   db.collection("finances")
