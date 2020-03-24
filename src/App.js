684e4381fee8d614011c13108b449986d163a382
Katerina <katerinalupacheva@gmail.com>
2020-03-24 15:08:28 +0300

    Redirect to home when logged in

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
+import jwtDecode from "jwt-decode";
 
 import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 import Sidebar from "./components/sidebar";
@@ -9,6 +10,7 @@ import LoginPage from "./pages/login-page";
 import MonthPage from "./pages/month-page";
 import PivotTablePage from "./pages/pivot-table-page";
 import ChartsPage from "./pages/charts-page";
+import AuthRoute from "./utils/authroute";
 
 const theme = createMuiTheme({
   palette: {
@@ -26,14 +28,25 @@ const theme = createMuiTheme({
 });
 
 function App() {
+  let authenticated;
+  const token = localStorage.FBIdToken;
+  if (token) {
+    const decodedToken = jwtDecode(token);
+    if (decodedToken.exp * 1000 < Date.now()) {
+      window.location.href = "/login";
+      authenticated = false;
+    } else {
+      authenticated = true;
+    }
+  }
   return (
     <MuiThemeProvider theme={theme}>
       <Router>
         <div className="App">
           <Sidebar>
             <Switch>
-              <Route path="/signup" component={SignupPage} />
-              <Route path="/login" component={LoginPage} />
+              <AuthRoute path="/signup" component={SignupPage} authenticated />
+              <AuthRoute path="/login" component={LoginPage} authenticated />
               <Route path="/month" component={MonthPage} />
               <Route path="/table" component={PivotTablePage} />
               <Route path="/charts" component={ChartsPage} />
