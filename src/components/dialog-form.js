2288a49127f55c995a1854ae0f1baba89d104ce6
Katerina <katerinalupacheva@gmail.com>
2020-03-24 17:33:34 +0300

    Default case added to dialog-form

--- src/components/dialog-form.js
+++ src/components/dialog-form.js
@@ -63,6 +63,8 @@ const DialogForm = ({ open, handleClose, handleSubmit }) => {
       case "category":
         handleCategoryChange(e.target.value);
         break;
+      default:
+        return;
     }
   };
 
