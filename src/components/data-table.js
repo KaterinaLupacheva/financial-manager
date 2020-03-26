32009da51d1e9f1feb36b9935e3b9c57c1e7fbee
Katerina <katerinalupacheva@gmail.com>
2020-03-26 15:45:58 +0300

    SnackBar added

--- src/components/data-table.js
+++ src/components/data-table.js
@@ -1,34 +1,64 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import MaterialTable from "material-table";
 import { TableContainer, Paper } from "@material-ui/core";
 import MonthExpensesContext from "../contexts/monthExpenses.context";
+import axios from "axios";
+import Snackbar from "./snackbar";
 
 const Table = ({ isExpenses }) => {
   const { expensesData } = useContext(MonthExpensesContext);
+  const [message, setMessage] = useState("");
+  const [snackbarIsOpened, openSnackbar] = useState(false);
   const name = isExpenses ? "Expenses" : "Income";
   return (
-    <TableContainer component={Paper} elevation={5}>
-      <MaterialTable
-        columns={[
-          { title: "Date", field: "date" },
-          { title: "Sum", field: "sum" },
-          { title: `${name}`, field: "details" },
-          { title: "Category", field: "category" }
-        ]}
-        data={isExpenses ? expensesData.combinedArrays : ""}
-        parentChildData={(row, rows) => rows.find(a => a.id === row.parentId)}
-        options={{
-          toolbar: false,
-          paging: false,
-          headerStyle: {
-            backgroundColor: "#9c27b0",
-            color: "#FFF",
-            fontWeight: "bold"
-          }
-        }}
-        style={{ borderBottom: "1px solid black" }}
+    <>
+      <TableContainer component={Paper} elevation={5}>
+        <MaterialTable
+          columns={[
+            { title: "Date", field: "date" },
+            { title: "Sum", field: "sum" },
+            { title: `${name}`, field: "details" },
+            { title: "Category", field: "category" }
+          ]}
+          data={isExpenses ? expensesData.combinedArrays : ""}
+          parentChildData={(row, rows) => rows.find(a => a.id === row.parentId)}
+          editable={{
+            onRowDelete: oldData =>
+              new Promise((resolve, reject) => {
+                setTimeout(() => {
+                  {
+                    axios
+                      .delete(`/expenses/${oldData.expenseId}`)
+                      .then(res => {
+                        openSnackbar(true);
+                        setMessage(res.data.message);
+                      })
+                      .catch(err => {
+                        console.error(err);
+                      });
+                  }
+                  resolve();
+                }, 1000);
+              })
+          }}
+          options={{
+            toolbar: false,
+            paging: false,
+            headerStyle: {
+              backgroundColor: "#9c27b0",
+              color: "#FFF",
+              fontWeight: "bold"
+            }
+          }}
+          style={{ borderBottom: "1px solid black" }}
+        />
+      </TableContainer>
+      <Snackbar
+        isOpened={snackbarIsOpened}
+        message={message}
+        handleSnackBarClose={() => openSnackbar(false)}
       />
-    </TableContainer>
+    </>
   );
 };
 
