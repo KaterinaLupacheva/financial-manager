3cddcfa222cde8df77f333f346adc154bc9059a1
Katerina <katerinalupacheva@gmail.com>
2020-03-29 22:52:25 +0300

    Bar chart styled

--- src/components/charts/grouped-bar-chart.js
+++ src/components/charts/grouped-bar-chart.js
@@ -1,39 +1,27 @@
-import React, { useState, useEffect } from "react";
-
+import React from "react";
+import { useTheme } from "@material-ui/core/styles";
 import { Bar } from "react-chartjs-2";
 
 const GroupedBarChart = ({ dataForChart }) => {
-  //   console.log("Chart " + JSON.stringify(dataForChart, null, 2));
+  const theme = useTheme();
   const data = {
     labels: dataForChart.labels,
     datasets: [
       {
         label: "Incomes",
-        backgroundColor: "rgba(255,99,132,0.2)",
-        borderColor: "rgba(255,99,132,1)",
-        borderWidth: 1,
-        //stack: 1,
-        hoverBackgroundColor: "rgba(255,99,132,0.4)",
-        hoverBorderColor: "rgba(255,99,132,1)",
+        backgroundColor: theme.palette.primary.light,
+        hoverBackgroundColor: theme.palette.primary.dark,
         data: dataForChart.incomes
       },
 
       {
         label: "Expenses",
-        backgroundColor: "rgba(155,231,91,0.2)",
-        borderColor: "rgba(255,99,132,1)",
-        borderWidth: 1,
-        //stack: 1,
-        hoverBackgroundColor: "rgba(255,99,132,0.4)",
-        hoverBorderColor: "rgba(255,99,132,1)",
+        backgroundColor: theme.palette.secondary.light,
+        hoverBackgroundColor: theme.palette.secondary.dark,
         data: dataForChart.expenses
       }
     ]
   };
-
-  useEffect(() => {
-    console.log(JSON.stringify(dataForChart, null, 2));
-  }, []);
 
   return <Bar data={data} />;
 };
