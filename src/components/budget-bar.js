d4a9fc9652bd3c787606342d4eab68e397f5653f
Katerina <katerinalupacheva@gmail.com>
2020-04-05 21:11:49 +0300

    Budget bar styling

--- src/components/budget-bar.js
+++ src/components/budget-bar.js
@@ -1,32 +1,55 @@
 import React, { useState, useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import LinearProgress from "@material-ui/core/LinearProgress";
+import Typography from "@material-ui/core/Typography";
+import Divider from "@material-ui/core/Divider";
 
 const useStyles = makeStyles(theme => ({
   root: {
     width: "100%",
     height: "10px",
-    marginTop: theme.spacing(2)
+    margin: "10px 0 20px 0"
+  },
+  dataContainer: {
+    display: "flex",
+    justifyContent: "space-between",
+    margin: "40px 0 0 20px"
+  },
+  rightContainer: {
+    display: "flex",
+    flexDirection: "column"
   }
 }));
 
 const BudgetBar = ({ data }) => {
   const classes = useStyles();
   const [completed, setCompleted] = useState(0);
+  const [remaining, setRemaining] = useState(0);
 
   useEffect(() => {
     const calculateCompleted =
       (parseInt(data.spent) / parseInt(data.budget)) * 100;
     setCompleted(calculateCompleted);
+
+    const calcuateRemaining = parseInt(data.budget) - parseInt(data.spent);
+    setRemaining(calcuateRemaining);
   }, []);
 
   return (
-    <div>
+    <div style={{ width: "50vw" }}>
+      <div className={classes.dataContainer}>
+        <Typography variant="h6">{data.category}</Typography>
+        <div className={classes.rightContainer}>
+          <Typography variant="caption">{"Remaining / Budget"}</Typography>
+          <Typography>{`${remaining} / ${data.budget}`}</Typography>
+        </div>
+      </div>
       <LinearProgress
         className={classes.root}
         variant="determinate"
         value={completed}
       />
+      <Divider />
     </div>
   );
 };
