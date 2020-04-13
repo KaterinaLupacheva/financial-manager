6026b078aed257d895ad4d325d445cb6270934d9
Katerina <katerinalupacheva@gmail.com>
2020-04-14 01:02:27 +0300

    Util method to save data to firestore - doc per month

--- src/utils/dataToFirestore.js
+++ src/utils/dataToFirestore.js
@@ -1,8 +1,9 @@
 import axios from "axios";
+import { format } from "date-fns";
 
-export const dataToFirestore = data => {
+export const dataToFirestore = (data) => {
   let tempData = [];
-  data.forEach(d => {
+  data.forEach((d) => {
     if (d.category !== "") {
       const tempDate = d.date.split("-");
       const year = tempDate[0];
@@ -14,7 +15,7 @@ export const dataToFirestore = data => {
         sum: d.sum,
         details: d.expenses,
         category: d.category,
-        email: "kat@tut.by"
+        email: "kat@tut.by",
       };
 
       // setTimeout(() =>
@@ -31,4 +32,94 @@ export const dataToFirestore = data => {
   });
 
   console.log(JSON.stringify(tempData, null, 2));
+};
+
+const saveExpense = (expense) => {
+  axios
+    .post(
+      "http://localhost:5000/financial-manager-271220/us-central1/api/expenses/",
+      expense
+    )
+    .then((res) => {
+      console.log("Saved");
+    })
+    .catch((err) => {
+      console.log("Error " + err);
+    });
+};
+
+const saveIncome = (income) => {
+  axios
+    .post(
+      "http://localhost:5000/financial-manager-271220/us-central1/api/incomes/",
+      income
+    )
+    .then((res) => {
+      console.log("Saved");
+    })
+    .catch((err) => {
+      console.log("Error " + err);
+    });
+};
+
+export const newExpensesDataToFirestore = (data) => {
+  let result = [];
+  data.reverse().forEach((d) => {
+    const tempDate = d.date.split("-");
+    const year = tempDate[0];
+    const month = tempDate[1] - 1;
+
+    result.push({
+      docId: format(new Date(year, month, 1, 8), "MMM-yyyy"),
+      date: new Date(year, month, 1, 8),
+      id: d.expenseId,
+      expenseDate: d.date,
+      sum: d.sum,
+      details: d.details,
+      category: d.category,
+    });
+  });
+
+  let index = -1;
+  let intervalId = setInterval(() => {
+    ++index;
+    if (index <= result.length) {
+      saveExpense(result[index]);
+    }
+  }, 3000);
+
+  if (index > result.length) {
+    clearInterval(intervalId);
+  }
+};
+
+export const newIncomeDataToFirestore = (data) => {
+  let result = [];
+  data.reverse().forEach((d) => {
+    const tempDate = d.date.split("-");
+    const year = tempDate[0];
+    const month = tempDate[1] - 1;
+
+    result.push({
+      docId: format(new Date(year, month, 1, 8), "MMM-yyyy"),
+      date: new Date(year, month, 1, 8),
+      id: d.incomeId,
+      incomeDate: d.date,
+      sum: d.sum,
+      details: d.details,
+      category: d.category,
+    });
+  });
+
+  let index = -1;
+  let intervalId = setInterval(() => {
+    ++index;
+    if (index <= result.length) {
+      saveIncome(result[index]);
+    }
+  }, 3000);
+
+  if (index > result.length) {
+    clearInterval(intervalId);
+  }
 };
