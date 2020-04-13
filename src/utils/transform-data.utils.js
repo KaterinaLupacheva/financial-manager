3676f64801e41855ed94e07f6a1a80eec26ea0ec
Katerina <katerinalupacheva@gmail.com>
2020-04-13 19:21:22 +0300

    Generate id util function added

--- src/utils/transform-data.utils.js
+++ src/utils/transform-data.utils.js
@@ -187,3 +187,13 @@ export const createTableRows = data => {
   }
   return rows;
 };
+
+export const generateId = () => {
+  const chars =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let autoId = "";
+  for (let i = 0; i < 15; i++) {
+    autoId += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return autoId;
+};
