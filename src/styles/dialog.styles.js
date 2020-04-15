7a0f5313f40d04cf3267f9546414c680a551a1ff
Katerina <katerinalupacheva@gmail.com>
2020-04-15 22:04:45 +0300

    Edit budget mobile styles

--- src/styles/dialog.styles.js
+++ src/styles/dialog.styles.js
@@ -1,31 +1,43 @@
-export const dialogStyles = theme => ({
+export const dialogStyles = (theme) => ({
   toggleContainer: {
-    margin: theme.spacing(2, 0)
+    margin: theme.spacing(2, 0),
   },
-  toggleButton: props => ({
+  toggleButton: (props) => ({
     "&.Mui-selected": {
       backgroundColor:
         props.bgcolor === "expenses"
           ? `${theme.palette.secondary.palePink} !important`
-          : `${theme.palette.secondary.lightBg} !important`
-    }
+          : `${theme.palette.secondary.lightBg} !important`,
+    },
   }),
+  row: {
+    display: "flex",
+    alignItems: "baseline",
+    justifyContent: "space-between",
+    flexWrap: "wrap",
+    [theme.breakpoints.down("md")]: {
+      justifyContent: "center",
+    },
+  },
   formControl: {
-    minWidth: 195
+    minWidth: 195,
   },
   sumField: {
-    marginLeft: 20
+    marginLeft: 20,
   },
   detailsField: {
-    marginRight: 20
+    marginRight: 20,
   },
   buttons: {
-    marginTop: 20
+    marginTop: 20,
   },
   button: {
-    minWidth: "20%"
+    minWidth: "20%",
   },
   budgetField: {
-    margin: 8
-  }
+    margin: 8,
+    [theme.breakpoints.down("md")]: {
+      marginBottom: 20,
+    },
+  },
 });
